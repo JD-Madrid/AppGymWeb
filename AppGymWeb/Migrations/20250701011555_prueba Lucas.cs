4349c2bbfa6a6afa98701bab7617c424a071a8f5
Nightmere <lucas.villani@hotmail.com.ar>
2025-07-01 09:17:01 -0300

    Cambios en el diseño

---
+++ AppGymWeb/Migrations/20250701011555_prueba Lucas.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppGymWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class pruebaLucas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
